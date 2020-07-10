7b4044e929366297ba73ecdd1ee75a96b0e00de0
Anonymous CCP Employee <anonymous.employee@ccpgames.com>
2020-07-10 10:52:05 +0000

    https://jira.ccpgames.com/browse/EO-2873
    Clean up IME.
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/packages/inputmethod/": change = 1768182]

--- python/inputmethod/test/__init__.py
+++ python/inputmethod/test/__init__.py
@@ -11,10 +11,6 @@ class TestIME(unittest2.TestCase):
     def tearDown(self):
         del self.ime
 
-    def testActivateKeyboardLayout(self):
-        ret = self.ime.ActivateKeyboardLayout(0, 0)
-        self.assertIsNone(ret)
-
     def testAssociateContext(self):
         ret = self.ime.AssociateContext(0)
         self.assertIsNone(ret)
@@ -23,54 +19,21 @@ class TestIME(unittest2.TestCase):
         ret = self.ime.Backspace(13)
         self.assertIsNone(ret)
 
-    def testDisableTextFrameService(self):
-        ret = self.ime.DisableTextFrameService()
-        self.assertIsNone(ret)
-
-    def testGetCandidateList(self):
-        ret = self.ime.GetCandidateList()
-        self.assertIsNone(ret)
-
     def testGetCompositionString(self):
         ret = self.ime.GetCompositionString(0)
         self.assertEqual(ret, {})
-
-    def testGetConversionList(self):
-        ret = self.ime.GetConversionList(u'a', 0)
-        self.assertIsNone(ret)
 
     def testGetConversionStatus(self):
         ret = self.ime.GetConversionStatus()
         self.assertListEqual(ret, [0, 0])
 
-    def testGetGuideLine(self):
-        ret = self.ime.GetGuideLine(0)
-        self.assertEqual(ret, 0)
-
-    def testGetIMEFileName(self):
-        ret = self.ime.GetIMEFileName()
-        self.assertEqual(ret, '')
-
-    def testGetImeId(self):
-        ret = self.ime.GetImeId()
-        self.assertEqual(ret, 0)
-
     def testGetKeyboardLayout(self):
         ret = self.ime.GetKeyboardLayout()
         self.assertIsInstance(ret, int)
 
-    def testGetKeyboardLayoutList(self):
-        ret = self.ime.GetKeyboardLayoutList()
-        self.assertIsInstance(ret, list)
-        self.assertGreater(len(ret), 0)
-
     def testGetOpenStatus(self):
         ret = self.ime.GetOpenStatus()
         self.assertEqual(ret, 0)
-
-    def testGetReadingString(self):
-        ret = self.ime.GetReadingString()
-        self.assertDictEqual(ret, {'bShowReadingWindow': 0})
 
     def testGetRefCounts(self):
         ret = self.ime.GetRefCounts()
@@ -84,18 +47,6 @@ class TestIME(unittest2.TestCase):
         ret = self.ime.NotifyIME(0, 0)
         self.assertEqual(ret, 0)
 
-    def testOnLanguageChanged(self):
-        ret = self.ime.OnLanguageChanged()
-        self.assertIsNone(ret)
-
-    def testSetCompositionString(self):
-        ret = self.ime.SetCompositionString(0, u'a', u'b')
-        self.assertEqual(ret, 0)
-
-    def testSetConversionStatus(self):
-        ret = self.ime.SetConversionStatus(0, 0)
-        self.assertIsNone(ret)
-
     def testSetHWND(self):
         ret = self.ime.SetHWND(0)
         self.assertIsNone(ret)
@@ -103,10 +54,6 @@ class TestIME(unittest2.TestCase):
     def testSetOpenStatus(self):
         ret = self.ime.SetOpenStatus(0)
         self.assertEqual(ret, 0)
-
-    def testShowReadingWindow(self):
-        ret = self.ime.ShowReadingWindow(0)
-        self.assertIsNone(ret)
 
     def testSimulateHotKey(self):
         ret = self.ime.SimulateHotKey(0)
